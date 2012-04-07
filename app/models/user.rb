5f6ecbffbe026dca895a0f6b4f040cb62e051390
Christian Bowen Reddington <chris.reddington@googlemail.com>
2012-04-07 19:55:38 +0100

    User signup now uses validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
   has_secure_password
   validates_presence_of :password, :on => :create
+  validates :email, :presence => true, :uniqueness => { :case_sensitive => false },
+                        :format => {:with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i}
 end
