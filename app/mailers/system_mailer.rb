16ffc63ae28e3757adb35414a974ea8d1c6dc70e
Christian Bowen Reddington <chris.reddington@googlemail.com>
2012-04-10 17:30:18 +0100

    Bug fix on previous commit

--- app/mailers/system_mailer.rb
+++ app/mailers/system_mailer.rb
@@ -4,7 +4,7 @@ class SystemMailer < ActionMailer::Base
   def welcome_email(user)
     @user = user
     @hash = Digest::SHA1.hexdigest(@user.created_at.to_s + @user.id.to_s)
-    @url = "http://forum.reddingtons.co.uk/" + @user.id + "/confirm/" + @hash
+    @url = "http://forum.reddingtons.co.uk/" + @user.id.to_s + "/confirm/" + @hash
     mail(:to => user.email, :subject => "Welcome to the Forum")
   end
 end
