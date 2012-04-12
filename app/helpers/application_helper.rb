b16caaf71bd9f38aa9284105af727242dcc9c288
Christian Bowen Reddington <chris.reddington@googlemail.com>
2012-04-13 00:17:23 +0100

    Show unread count in main navigation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,6 +14,6 @@ module ApplicationHelper
     end
     
     def user_message_count
-      return UserConversation.count(:conditions => ['read != ? and user_id = ?', true, current_user.id]).to_s
+      return UserConversation.count(:conditions => ["'user_id' = ? AND 'read' IS NOT TRUE", current_user.id]).to_s
     end
 end
